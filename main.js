723c66a8dcb6bc70e26d0644e273b30ed2f4b39a
GBalintG <balintgheczy@gmail.com>
2025-02-18 23:21:59 +0100

    HáziFeladatMegoldása

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import { lnko_teszt } from "./teszt.js";
 
 export function lnko(a, b) {
+  a = Math.abs(a);
+  b = Math.abs(b);
+
+  if (a === 0 || b === 0) {
+    return 0;
+  }
+  
   while (a != b) {
     if (a > b) {
       a = a - b;
--- teszt.js
+++ teszt.js
@@ -18,12 +18,64 @@ export function lnko_teszt() {
       tesztesetNeve: "2. Teszteset: a és b nem prímek, a többszöröse b-nek.",
     },
     {
-      /*
-      a: 
-      b: 
-      vart: 
-      tesztesetNeve:
-      */
+      a: -15,
+      b: 9,
+      vart: 3,
+      tesztesetNeve: "3. Teszteset: a és b nem relatív prím."
+    },
+    {
+      a: 10,
+      b: 0,
+      vart: 0,
+      tesztesetNeve: "4. Teszteset: a és b nem relatív prím."
+    },
+    {
+      a: 0,
+      b: 10,
+      vart: 0,
+      tesztesetNeve: "5. Teszteset: a és b nem relatív prím." 
+    },
+    {
+      a: 15,
+      b: 9,
+      vart: 3,
+      tesztesetNeve: "6. Teszteset: a és b nem relatív prím."
+    },
+    {
+      a: 6,
+      b: 3,
+      vart: 3,
+      tesztesetNeve: "7. Teszteset: a és b nem relatív prím."
+    },
+    {
+      a: 9,
+      b: 15,
+      vart: 3,
+      tesztesetNeve: "8. Teszteset: a és b nem relatív prím."
+    },
+    {
+      a: 7,
+      b: 10,
+      vart: 1,
+      tesztesetNeve: "9. Teszteset: a és b nem relatív prím."
+    },
+    {
+      a: 15,
+      b: 8,
+      vart: 1,
+      tesztesetNeve: "10. Teszteset: a és b nem relatív prím."
+    },
+    {
+      a: 5,
+      b: 5,
+      vart: 5,
+      tesztesetNeve: "11. Teszteset: a és b nem relatív prím."
+    },
+    {
+      a: 8,
+      b: 8,
+      vart: 8,
+      tesztesetNeve: "12. Teszteset: a és b nem relatív prím."
     },
   ];
 
@@ -32,11 +84,16 @@ export function lnko_teszt() {
     let b = tesztObjLista[index].b;
     let vart = tesztObjLista[index].vart;
     let kapott = lnko(a, b);
+
+    console.log(`${index+1}. teszteset - a és b nem relatív prímek`)
+    console.log(`a: ${a} b: ${b} várt: ${vart} kapott: ${kapott}`)
+
     console.assert(
       vart === kapott,
       "%o",
       `a = ${a}, b = ${b}, vart = ${kapott}`,
       `${tesztObjLista[index].tesztesetNeve}`
     );
+    console.log(``)
   }
 }
